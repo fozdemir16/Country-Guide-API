77f7607cca57373d97db1089f712620cb5c54459
fozdemir16 <fozdemir8c@gmail.com>
2022-11-01 12:48:59 -0400

    count1

---
+++ script.js
@@ -0,0 +1,22 @@
+let searchBtn = document.getElementById("search");
+let countryInp = document.getElementById("country");
+
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInp.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+        //     console.log(data[0]);
+      //   console.log(data[0].capital[0]);
+      //   console.log(data[0].flags.svg);
+      //   console.log(data[0].name.common);
+      //   console.log(data[0].continents[0]);
+      //   console.log(Object.keys(data[0].currencies)[0]);
+      //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+      //   console.log(
+      //     Object.values(data[0].languages).toString().split(",").join(", ")
+      //   );
+        
+        })
+})
